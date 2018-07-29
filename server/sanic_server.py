5ca3a989772bbe88a7225cf8f94d541e52d0b2e7
lagicrus <Lagicrus@users.noreply.github.com>
2018-07-29 19:57:59 +0100

    Major Update
    Removed old route "/" handler and replaced it with the static route
    
    Started implementation of the messages GET and POST handlers.

--- server/sanic_server.py
+++ server/sanic_server.py
@@ -7,19 +7,22 @@ app = Sanic("chator")
 print("Setup App")
 
 app.static("/favicon.ico", "./favicon.ico", name="favicon")
-app.static("/html", "./webpages/index.html")
+app.static("/", "./webpages/index.html")
 app.static("/py-chator.js", "./webpages/py-chator.js")
 app.static("/css/index.css", "./webpages/index.css")
 
-@app.route("/")
-async def main_handler(request):
-    return sanic.response.text("Hi")
 
-"""
-@app.route("/html")
-async def html_handler(request):
-    return sanic.response.html("")
-"""
+@app.route("/messages/get", methods=["GET"])
+async def messages_handler(request):
+    # Get the messages from the database
+    return sanic.response.text("hi")
+
+
+@app.route("/messages/post", methods=["POST"])
+async def messages_post_handler(request):
+    # Add the message to the database
+    return sanic.response.text("You have posted")
+
 
 if __name__ == "__main__":
     ip = requests.get("http://api.ipify.org")
