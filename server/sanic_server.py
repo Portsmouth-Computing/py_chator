1ab3e69920a1275a8cd6441b25b9f865f8868fe9
lagicrus <Lagicrus@users.noreply.github.com>
2018-07-29 19:08:09 +0100

    Added web
    Added pathing and static requests for requesting files

--- server/sanic_server.py
+++ server/sanic_server.py
@@ -1,15 +1,27 @@
 from sanic import Sanic
 import sanic.response
+import requests
+print("Imported")
 
 app = Sanic("chator")
+print("Setup App")
 
 app.static("/favicon.ico", "./favicon.ico", name="favicon")
-
+app.static("/html", "./webpages/index.html")
+app.static("/py-chator.js", "./webpages/py-chator.js")
+app.static("/css/index.css", "./webpages/index.css")
 
 @app.route("/")
 async def main_handler(request):
     return sanic.response.text("Hi")
 
+"""
+@app.route("/html")
+async def html_handler(request):
+    return sanic.response.html("")
+"""
 
 if __name__ == "__main__":
+    ip = requests.get("http://api.ipify.org")
+    print(f"Running on {ip.text}")
     app.run(host="0.0.0.0", port=80, access_log=True)
