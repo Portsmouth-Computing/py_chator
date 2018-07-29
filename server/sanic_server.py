409eeec952918a0fff99f4002990ec251c3f1d10
lagicrus <Lagicrus@users.noreply.github.com>
2018-07-29 18:25:56 +0100

    sanic_server test

--- server/sanic_server.py
+++ server/sanic_server.py
@@ -3,3 +3,13 @@ import sanic.response
 
 app = Sanic("chator")
 
+app.static("/favicon.ico", "./favicon.ico", name="favicon")
+
+
+@app.route("/")
+async def main_handler(request):
+    return sanic.response.text("Hi")
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=80, access_log=True)
