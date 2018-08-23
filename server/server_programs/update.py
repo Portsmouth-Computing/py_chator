1df68a1381b7829f905f4435e08e0479a682a4db
Lagicrus <Lagicrus@users.noreply.github.com>
2018-08-23 20:24:58 +0100

    more configurable locations

--- server/server_programs/update.py
+++ server/server_programs/update.py
@@ -4,26 +4,29 @@ import os.path
 import logging
 log = logging.getLogger(__name__)
 
+VERSION_JSON_LOCATION = "./version.json"
+BOOTSTRAP_JS_LOCATION = "../webpages/bootstrap.min.js"
+
 
 async def bootstrap_version_check():
     bootstrap_version = requests.get("https://api.github.com/repos/twbs/bootstrap/releases")
     latest_json = bootstrap_version.json()[0]
-    if os.path.isfile("./version.json") is False:
-        log.warning("./version.json doesn't exist, creating.")
-        with open("./versino.json", "w") as file:
+    if os.path.isfile(VERSION_JSON_LOCATION) is False:
+        log.warning(f"{VERSION_JSON_LOCATION} doesn't exist, creating.")
+        with open(VERSION_JSON_LOCATION, "w") as file:
             file.write(str({"version": "0.0"}))
-        log.info("./version.json created.")
-    if os.path.isfile("../webpages/bootstrap.min.js") is False:
-        log.warning("../webpages/bootstrap.min.js doesn't exist, creating.")
-        with open("../webpages/bootstrap.min.js", "w+") as file:
+        log.info(f"{VERSION_JSON_LOCATION} created.")
+    if os.path.isfile(BOOTSTRAP_JS_LOCATION) is False:
+        log.warning(f"{BOOTSTRAP_JS_LOCATION} doesn't exist, creating.")
+        with open(BOOTSTRAP_JS_LOCATION, "w+") as file:
             file.write("")
-        log.info("../webpages/bootstrap.min.js created.")
-    with open("./version.json") as file:
+        log.info(f"{BOOTSTRAP_JS_LOCATION} created.")
+    with open(VERSION_JSON_LOCATION) as file:
         json_file = json.load(file)
     if latest_json["name"] != json_file["version"]:
         log.info("Updating bootstrap file")
         latest_bootstrap_js = requests.get("https://stackpath.bootstrapcdn.com/bootstrap/{}/js/bootstrap.min.js".format(latest_json["name"][1:]))
-        with open("../webpages/bootstrap.min.js", "w") as file:
+        with open(BOOTSTRAP_JS_LOCATION, "w") as file:
             file.write(str(latest_bootstrap_js))
         log.info("Updated bootstrap file")
     else:
